94b9d120e554a55932a8b202bf2f2097c3fd6432
JaviLorbada <javugi@gmail.com>
2017-05-22 17:24:05 +0200

    Setup MapViewController UI.

--- Twidder/Views/MapViewController.swift
+++ Twidder/Views/MapViewController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 import MapKit
-
-struct MapViewModel { }
+import ReactiveCocoa
+import ReactiveSwift
 
 final class MapViewController: UIViewController {
 
   fileprivate let viewModel: MapViewModel
   fileprivate let searchBar: UISearchBar
   fileprivate let mapView: MKMapView
+  fileprivate let locationButton: LocationButton
   fileprivate lazy var views: [UIView] = {
-    return [self.mapView, self.searchBar]
+    return [self.mapView, self.searchBar, self.locationButton]
   }()
   
   // MARK: - Lifecycle
@@ -25,37 +26,45 @@ final class MapViewController: UIViewController {
     self.viewModel = viewModel
     self.searchBar = UISearchBar()
     self.mapView = MKMapView()
+    self.locationButton = LocationButton()
     
     super.init(nibName: nil, bundle: nil)
     
+    setupViews()
     setupConstraints()
+    setupBindings()
   }
   
   @available(*, unavailable) required init?(coder aDecoder: NSCoder) {
     fatalError("this is a xibless class utilizing for autolayout, use init() instead")
   }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-  }
   
   // MARK: - Private
-  private func setupConstraints() {
+  private func setupViews() {
     for v in views {
       view.addSubview(v)
       v.translatesAutoresizingMaskIntoConstraints = false
     }
+    searchBar.delegate = self
+    mapView.delegate = self
+  }
+  
+  private func setupConstraints() {
     
     NSLayoutConstraint.activate([
+      mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      mapView.topAnchor.constraint(equalTo: view.topAnchor),
+      mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      
       searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
       searchBar.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
       searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
       
-      mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      mapView.topAnchor.constraint(equalTo: view.topAnchor),
-      mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+      locationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      locationButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+      locationButton.widthAnchor.constraint(equalToConstant: 50),
+      locationButton.heightAnchor.constraint(equalTo: locationButton.widthAnchor)
     ])
   }
   
